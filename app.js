1901fb7b0c8c22a70f160313610d20bf745fc30f
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-21 10:46:55 +0200

    add nodemon npm start

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 
 const path = require('./util/path');
-const connection = require('./database/db');
-const petRoutes = require('./routes/petRoutes');;
+const petRoutes = require('./routes/petRoutes');
 
 const app = express();
 
-app.use(express.json().urlencoded({
-    extended: false
-}));
-
+app.use(express.json());
 
 app.use('/api/pets', petRoutes);
 
