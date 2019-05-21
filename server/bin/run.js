e686b971c09c8dcb0dc6962bbb21632141a7267f
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-21 12:22:30 +0200

    create path.js, config.js, edit run.js

--- server/bin/run.js
+++ server/bin/run.js
@@ -1,12 +1,9 @@
 "use strict";
 
 const app = require('../../app');
-const server = require('../../util/config').server;
+const serverPort = require('../../util/config');
 
 
-const port = (server.port || 3000);
-
-
-app.listen(port, () => {
-    console.log(`Express server started, listening on port ${port}`);
+app.listen((serverPort || 3000), () => {
+    console.log(`Express server started, listening on port ${serverPort}`);
 });
---
+++ util/config.js
@@ -0,0 +1,7 @@
+const dotenv = require('dotenv').config();
+
+
+var serverPort = process.env.SERVER_PORT;
+
+
+module.exports = serverPort;
