a577cf2a13d60dcd1289d6d4d108b829e9641a82
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-21 17:05:30 +0200

    add multer config

--- util/multer-config.js
+++ util/multer-config.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const storage = multer.memoryStorage();
 
-const multerUpload = multer({storage: storage});
+const upload = multer({storage: storage});
 
 
-module.exports = multerUpload;
+module.exports = upload;
