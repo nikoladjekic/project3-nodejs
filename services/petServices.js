fe61bb4488ea1bc8ed83300c5a8095099b9ff5f8
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-21 12:39:47 +0200

    create Pet Routes and blueprint for services

---
+++ services/petServices.js
@@ -0,0 +1,16 @@
+const logger = require('../util/logger');
+
+const createPet, getAllPets, getPetById, updatePetById, deletePet;
+
+
+
+
+
+
+module.exports = {
+    createPet,
+    getPetById,
+    getAllPets,
+    updatePetById,
+    deletePet
+}
