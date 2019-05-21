79bdb15a93e06a7bc2236010d5cb304b9cc510d8
Tamara Dragovic <Tamara.Dragovic@eng.it>
2019-05-21 10:27:21 +0200

    Add DB connection

--- database/db.js
+++ database/db.js
@@ -1,1 +1,14 @@
-//db
+var mongoose = require('mongoose');
+const database = require('../util/config').database;
+
+const db_url = 'mongodb+srv://' + database.user + ':' + database.password + '@nodeproject-tu8wo.mongodb.net/'+ database.name +'?retryWrites=true';
+
+
+const connection = mongoose.connect(db_url, { 
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+
+module.exports = connection;
