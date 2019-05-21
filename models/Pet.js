1fa5ed62b0f6a102180a98f79ebedcd449f00a83
Aleksandar Stojmenovic <aleksandar.stojmenovic9@gmail.com>
2019-05-21 10:56:49 +0200

    Create Pet model

--- models/Pet.js
+++ models/Pet.js
@@ -1,1 +1,31 @@
-//pet model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+    name: {
+        type: String
+    }
+});
+
+
+const PetSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    category: {
+        type: CategorySchema
+    },
+    status: {
+        type: String,
+        uppercase: true,
+        enum: ["AVAILABLE", "PENDING", "SOLD"],
+        default: "AVAILABLE"
+    },
+
+});
+
+let PetModel = mongoose.model('Pet', PetSchema);
+
+module.exports = PetModel;
