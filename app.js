04a125204e34fb7801625d101ae796a32182134b
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-21 10:35:58 +0200

    create app.js and run.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//test
+const express = require('express');
+
+const path = require('./util/path');
+const connection = require('./database/db');
+const petRoutes = require('./routes/petRoutes');;
+
+const app = express();
+
+app.use(express.json().urlencoded({
+    extended: false
+}));
+
+
+app.use('/api/pets', petRoutes);
+
+
+// if no route matches, send 404 response
+app.use((req, res, next) => {
+    res.status(404).send('Page not found');
+});
+
+module.exports = app;
