a7b59ec49bfbf6da15f90de25d083743b437f57f
Nikola Djekic <nikoladj.21@gmail.com>
2019-05-22 10:25:42 +0200

    implement multer

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -1,10 +1,23 @@
 const express = require('express');
-const { forwardJsonPetData, deletePetJson, updatePetJson } = require('../services/sendPetDataToMongoJSON');
+const {
+    forwardJsonPetData,
+    deletePetJson,
+    updatePetJson
+} = require('../services/sendPetDataToMongoJSON');
+
+const upload = require('../util/multer-config');
+const csv = require('csvtojson');
 
 const router = express.Router();
 
 router.post('/json', forwardJsonPetData);
 router.delete('/json/:id', deletePetJson);
 router.put('/json', updatePetJson);
+
+
+router.post("/file", upload.single('fileUpload'), (req, res) => {
+    console.log(req.file);
+    res.send(req.file.buffer.toString());
+});
 
 module.exports = router;
--- util/multer-config.js
+++ util/multer-config.js
@@ -1,8 +1,28 @@
 const multer = require('multer');
 
+// * allocate storage to buffer memory *
 const storage = multer.memoryStorage();
 
-const upload = multer({storage: storage});
+// * accept only txt and csv file types *
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'text/plain' || file.mimetype === 'text/csv') {
+        cb(null, true);
+    } else {
+        cb(new Error('File type not supported'), false);
+    }
+};
+
+// * accept files only up to 5 MB *
+const maxSize = 1024 * 1024 * 5;
+
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: maxSize
+    },
+    fileFilter: fileFilter
+});
 
 
 module.exports = upload;
